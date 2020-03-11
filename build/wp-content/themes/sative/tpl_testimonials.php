cc266800aa027c542d8f555111c6733f07d4e459
Jakub Pabis <jakubpabis@Jakubs-MBP.home>
2020-03-12 00:16:34 +0100

    testimonials mobiler ready

--- build/wp-content/themes/sative/tpl_testimonials.php
+++ build/wp-content/themes/sative/tpl_testimonials.php
@@ -11,12 +11,12 @@ get_template_part( 'template-parts/breadcrumbs' ); ?>
 <header class="header__testimonials dog section left sea">
     <div class="container">
         <div class="row justify-content-between">
-            <div class="col-lg-3">
+            <div class="col-3">
                 <div class="thedog dog__stand left">
                     <svg viewBox="0 0 306.39 610.05" xmlns="http://www.w3.org/2000/svg"><path d="M140.49 0l-28 28.13v138.49h41.39l25.41-25.41V79.32H169V137l-19.24 19.42h-26.88V32.31l22-22.15h151V53l-26.34 27.77H190.4v90.94l6.35 6.35h-55.17L112.9 212v258.11l-58.45 58.44H0v10.35h58.63l64.43-64.44V215.81l23.24-27.4h60.26V188l10 10v23.8h-54.29v-12h-10.35v22.33h84.22l22.51 22V320h-22.33l-19.42-19.42v-30.5h-39.75v10.35h29.41v24.5l10.7 10.71v225.07l-47.91 43.38-8.17-8.35 38.12-35-45.2-46.28-7.26 7.26 37.75 38.66-37.93 34.82 23.59 24.5H106v-84.22H95.65v94.57h98.74l-17.78-18.52 51.18-46.28V325.81l4.36 4.35H269v-80.41l-28-28.13h-13.75v-28.13l-26.32-26.32v-76h73.33l32.13-33.76V0z" fill="#173751"/><g class="bowtie" fill="#88d8e5"><path d="M225.42 206.72l-23.51-23.51 23.51-23.51 7.31 7.32-16.19 16.19 16.19 16.2z"/><path d="M193.2 206.72l-7.32-7.31 16.2-16.2-16.2-16.21 7.32-7.32 23.51 23.51z"/></g></svg>
                 </div>
             </div>
-            <div class="col-lg-8">
+            <div class="col-lg-8 col-9">
                 <div class="dog__info left bg-sea">
                     <?php if(get_field('candidates_title')): ?>
                     <h2 class="text-size-xxxxlarge">
@@ -59,13 +59,13 @@ $query = new WP_Query( $args );
                         <?php $i = 0; while($query->have_posts()) : $query->the_post(); ?>
                         <div class="carousel-item <?= $i == 0 ? 'active' : null ?>">
                             <div class="row justify-content-center">
-                                <div class="col-lg-9">
+                                <div class="col-sm-9 col-10">
                                     <div class="row">
                                         <?php if(has_post_thumbnail()): ?>
-                                        <div class="col-lg-2">
+                                        <div class="col-lg-2 col-md-3">
                                             <img data-src="<?= get_the_post_thumbnail_url(); ?>" alt="" class="bg-cover lazy">
                                         </div>
-                                        <div class="col-lg-10">
+                                        <div class="col-lg-10 col-md-9">
                                         <?php else: ?>
                                         <div class="col-12">
                                         <?php endif; ?>
@@ -84,17 +84,17 @@ $query = new WP_Query( $args );
                         <?php $i++; ?>
                         <?php endwhile; ?>
                     </div>
-                    <?php if($i > 1): ?>
-                    <a class="carousel-control-prev" href="#carouselCandidates" role="button" data-slide="prev">
-                        <i class="fas fa-caret-left"></i>
-                        <span class="sr-only">Previous</span>
-                    </a>
-                    <a class="carousel-control-next" href="#carouselCandidates" role="button" data-slide="next">
-                        <i class="fas fa-caret-right"></i>
-                        <span class="sr-only">Next</span>
-                    </a>
-                    <?php endif; ?>
                 </div>
+                <?php if($i > 1): ?>
+                <a class="carousel-control-prev" href="#carouselCandidates" role="button" data-slide="prev">
+                    <i class="fas fa-caret-left"></i>
+                    <span class="sr-only">Previous</span>
+                </a>
+                <a class="carousel-control-next" href="#carouselCandidates" role="button" data-slide="next">
+                    <i class="fas fa-caret-right"></i>
+                    <span class="sr-only">Next</span>
+                </a>
+                <?php endif; ?>
             </div>
         </div>
     </div>
@@ -105,7 +105,7 @@ $query = new WP_Query( $args );
 <section class="dog section grey2 right">
     <div class="container">
         <div class="row">
-            <div class="col-lg-8">
+            <div class="col-lg-8 col-sm-9 col-10">
                 <div class="dog__info right bg-grey2">
                     <?php if(get_field('clients_title')): ?>
                     <h2 class="text-size-xxxxlarge">
@@ -116,7 +116,7 @@ $query = new WP_Query( $args );
                 </div>
                 <div class="triangle grey2"></div>
             </div>
-            <div class="col-lg-4 thedog dog__normal right">
+            <div class="col-lg-4 col-sm-3 col-2 thedog dog__normal right">
                 <svg viewBox="0 0 649.89 364.92" xmlns="http://www.w3.org/2000/svg"><path d="M484.2 0l-28 28.09v138.59h41.45l25.37-25.37v-62h-10.23v57.79l-19.43 19.42h-27v-124l22-22.15H639.5v42.84l-26.11 26.11H534.2v99H170.76L63.11 285.84H0v10.28h67.44L175.1 188.58h359.1v91.94l-37.87 46.28 26.85 27.72h-56.8v-72.26H233.13l-46.53 44.42 26.73 27.72h-64.22V237h-10.27v127.79h98.75l-36.39-37.74 36-34.4h218.9v72.27h91.33l-37.25-38.49 34.4-41.95V89.84h73.13l32.17-32.05V.49H484.2" fill="#173751"/><g class="bowtie" fill="#88d8e5"><path d="M555.61 206.72l-23.4-23.39 23.4-23.4 7.22 7.22-16.18 16.18 16.18 16.17z"/><path d="M523.25 206.72L516 199.5l16.17-16.17L516 167.15l7.21-7.22 23.4 23.4-23.4 23.39"/></g></svg>
             </div>
         </div>
@@ -150,13 +150,13 @@ $query = new WP_Query( $args );
                         <?php $i = 0; while($query->have_posts()) : $query->the_post(); ?>
                         <div class="carousel-item <?= $i == 0 ? 'active' : null ?>">
                             <div class="row justify-content-center">
-                                <div class="col-lg-9">
+                                <div class="col-sm-9 col-10">
                                     <div class="row">
                                         <?php if(has_post_thumbnail()): ?>
-                                        <div class="col-lg-2">
+                                        <div class="col-lg-2 col-md-3">
                                             <img data-src="<?= get_the_post_thumbnail_url(); ?>" alt="" class="bg-cover lazy">
                                         </div>
-                                        <div class="col-lg-10">
+                                        <div class="col-lg-10 col-md-9">
                                         <?php else: ?>
                                         <div class="col-12">
                                         <?php endif; ?>
@@ -175,17 +175,17 @@ $query = new WP_Query( $args );
                         <?php $i++; ?>
                         <?php endwhile; ?>
                     </div>
-                    <?php if($i > 1): ?>
-                    <a class="carousel-control-prev" href="#carouselClients" role="button" data-slide="prev">
-                        <i class="fas fa-caret-left"></i>
-                        <span class="sr-only">Previous</span>
-                    </a>
-                    <a class="carousel-control-next" href="#carouselClients" role="button" data-slide="next">
-                        <i class="fas fa-caret-right"></i>
-                        <span class="sr-only">Next</span>
-                    </a>
-                    <?php endif; ?>
                 </div>
+                <?php if($i > 1): ?>
+                <a class="carousel-control-prev" href="#carouselClients" role="button" data-slide="prev">
+                    <i class="fas fa-caret-left"></i>
+                    <span class="sr-only">Previous</span>
+                </a>
+                <a class="carousel-control-next" href="#carouselClients" role="button" data-slide="next">
+                    <i class="fas fa-caret-right"></i>
+                    <span class="sr-only">Next</span>
+                </a>
+                <?php endif; ?>
             </div>
         </div>
     </div>
