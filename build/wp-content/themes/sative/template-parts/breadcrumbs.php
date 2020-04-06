b8e16538ac42731441dc8af9d667c6c2f11707df
Jakub Pabis <jakubpabis@Jakubs-MBP.home>
2020-04-06 13:17:15 +0200

    responsive breadcrumbs

--- build/wp-content/themes/sative/template-parts/breadcrumbs.php
+++ build/wp-content/themes/sative/template-parts/breadcrumbs.php
@@ -1,13 +1,13 @@
 <?php if ( function_exists('yoast_breadcrumb') ) : ?>
 <aside class="breadcrumbs">
     <div class="container">
-        <div class="row align-items-center">
+        <div class="row align-items-lg-center">
             <div class="col-auto">
                 <svg width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
                     <path fill-rule="evenodd" clip-rule="evenodd" d="M13.3651 15L9 10.6392L4.63516 15L3 13.3674L7.36529 9.00613L7.35915 8.99977L7.36529 8.99364L3.00045 4.63259L4.63471 3L9 7.36037L13.3653 3L14.9998 4.63259L10.6347 8.99364L10.6411 8.99977L10.6347 9.00613L15 13.3674L13.3651 15ZM0 18H18V0H0V18Z" fill="#94D4E9"/>
                 </svg>
             </div>
-            <div class="col-auto">
+            <div class="col-auto breadcrumbs__list">
                 <div class="breadcrumbs__items">
                 <?php $breadcrumbs = yoast_breadcrumb('','',false); 
                     $breadcrumbs = str_replace("<span>","",$breadcrumbs);
