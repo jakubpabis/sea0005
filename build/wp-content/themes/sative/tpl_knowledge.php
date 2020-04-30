48aa9843b58163855768b0e0a8509cc1fc155f1f
Jakub Pabis <jakubpabis@Jakubs-MBP.home>
2020-04-30 22:35:04 +0200

    some rwd

--- build/wp-content/themes/sative/tpl_knowledge.php
+++ build/wp-content/themes/sative/tpl_knowledge.php
@@ -69,11 +69,11 @@ $pagination = paginate_links( array(
             </div>
         </div>
         <?php endif; ?>
-        <div class="row align-items-center justify-content-between header__jobs-search knowledge">
+        <div class="row align-items-center justify-content-sm-between justify-content-end  header__jobs-search knowledge">
             <div class="header__jobs-dog">
                 <svg viewBox="0 0 649.89 364.92" xmlns="http://www.w3.org/2000/svg"><path d="M484.2 0l-28 28.09v138.59h41.45l25.37-25.37v-62h-10.23v57.79l-19.43 19.42h-27v-124l22-22.15H639.5v42.84l-26.11 26.11H534.2v99H170.76L63.11 285.84H0v10.28h67.44L175.1 188.58h359.1v91.94l-37.87 46.28 26.85 27.72h-56.8v-72.26H233.13l-46.53 44.42 26.73 27.72h-64.22V237h-10.27v127.79h98.75l-36.39-37.74 36-34.4h218.9v72.27h91.33l-37.25-38.49 34.4-41.95V89.84h73.13l32.17-32.05V.49H484.2" fill="#173751"/><g class="bowtie" fill="#88d8e5"><path d="M555.61 206.72l-23.4-23.39 23.4-23.4 7.22 7.22-16.18 16.18 16.18 16.17z"/><path d="M523.25 206.72L516 199.5l16.17-16.17L516 167.15l7.21-7.22 23.4 23.4-23.4 23.39"/></g></svg>
             </div>
-            <div class="offset-md-1 col-md-11 offset-sm-2 col-sm-10">
+            <div class="offset-md-1 col-md-11 offset-sm-2 col-sm-10 col-11 header__jobs-dog-search">
                 <div class="triangle-left"></div>
                 <input type="text" placeholder="<?php pll_e('Search our knowledge base. We have tons of useful articles for you!'); ?>">
                 <div class="gradient"></div>
@@ -151,7 +151,7 @@ $pagination = paginate_links( array(
                     <?= $pagination; ?>
                 </nav>
             </main>
-            <aside class="col-lg-4 col-md-12 col-sm-11 order-lg-12 order-1 knowledge__filters not-opened">
+            <aside class="col-lg-4 col-md-12 col-11 order-lg-12 order-1 knowledge__filters not-opened">
                 <div class="closethis">
                     <svg width="40" height="40" viewBox="0 0 40 40" fill="none" xmlns="http://www.w3.org/2000/svg">
                         <path fill-rule="evenodd" clip-rule="evenodd" d="M30.3884 34.2353L20.1176 23.9745L9.84745 34.2353L6 30.3939L16.2713 20.1321L16.2568 20.1171L16.2713 20.1027L6.00107 9.84138L9.84638 6L20.1176 16.2597L30.3889 6L34.2348 9.84138L23.964 20.1027L23.979 20.1171L23.964 20.1321L34.2353 30.3939L30.3884 34.2353ZM0 40H40V0H0V40Z" fill="#EC6278"/>
