4f0ef90ac1fc822f2c0cf124bc126be3b640182a
Jakub Pabis <jakubpabis@Jakubs-MacBook-Pro.local>
2020-05-25 19:33:22 +0200

    asdfdsf

--- build/wp-content/themes/sative/inc/fetchdata.php
+++ build/wp-content/themes/sative/inc/fetchdata.php
@@ -4,29 +4,37 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-function CallAPI($method, $url, $data = false)
-{
-    // kvstore API url
-    // Collection object
-    $data = [
-        'collection' => 'RapidAPI'
-    ];
-    // Initializes a new cURL session
-    $curl = curl_init($url);
-    // Set the CURLOPT_RETURNTRANSFER option to true
-    curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-    // Set the CURLOPT_POST option to true for POST request
-    curl_setopt($curl, CURLOPT_POST, true);
-    // Set custom headers for RapidAPI Auth and Content-Type header
-    curl_setopt($curl, CURLOPT_HTTPHEADER, [
-        'Content-Type: application/json'
-    ]);
-    // Execute cURL request with all previous settings
-    $response = curl_exec($curl);
-    // Close cURL session
+function callAPI($method, $url, $data = false){
+    $curl = curl_init();
+    switch ($method){
+        case "POST":
+            curl_setopt($curl, CURLOPT_POST, 1);
+            if ($data)
+                curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
+            break;
+        case "PUT":
+            curl_setopt($curl, CURLOPT_CUSTOMREQUEST, "PUT");
+            if ($data)
+                curl_setopt($curl, CURLOPT_POSTFIELDS, $data);			 					
+            break;
+        default:
+            if ($data)
+                $url = sprintf("%s?%s", $url, http_build_query($data));
+    }
+    // OPTIONS:
+    curl_setopt($curl, CURLOPT_URL, $url);
+    // curl_setopt($curl, CURLOPT_HTTPHEADER, array(
+    //     'APIKEY: 111111111111111111111',
+    //     'Content-Type: application/json',
+    // ));
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($curl, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
+    // EXECUTE:
+    $result = curl_exec($curl);
+    if(!$result){die("Connection Failure");}
     curl_close($curl);
-    echo $response . PHP_EOL;
-}
+    return $result;
+ }
 
 /**
  * Adding/updating jobs from XML
@@ -39,7 +47,7 @@ function userDataFetch()
 
     if( isset( $_GET['code'] ) ) {
         $url = 'https://github.com/login/oauth/access_token?client_id=3b1b9252c021bbb321e0&client_secret=5afd24b3d4d0bf252e8034139ec6a00bc2682367&code='.$_GET['code'];
-        CallAPI($url, 'POST');
+        var_dump( callAPI($url, 'POST') );
         //$redirect = $cookie.'?code='.$_GET['code'];
     }
     
