a560e4c01f1593fd2765752995cac7947673e51c
Jakub Pabis <jakubpabis@Jakubs-MacBook-Pro.local>
2020-05-25 21:08:38 +0200

    some

--- build/wp-content/themes/sative/inc/fetchdata.php
+++ build/wp-content/themes/sative/inc/fetchdata.php
@@ -56,7 +56,7 @@ function userDataFetch()
         ];
         $request = Requests::post('https://github.com/login/oauth/access_token', $headers, $options);
         echo '<pre>';
-        echo var_dump( $request );
+        echo var_dump( $request->body );
         echo '</pre>';
         //$redirect = $cookie.'?code='.$_GET['code'];
     }
