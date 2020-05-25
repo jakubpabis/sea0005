540c75f56c164a1e5cf60d9c1b4bbcf1fd381e43
Jakub Pabis <jakubpabis@Jakubs-MacBook-Pro.local>
2020-05-25 14:54:18 +0200

    again

--- prototype/dist/js/main.js
+++ prototype/dist/js/main.js
@@ -3534,7 +3534,7 @@ function afterFormOpen()
 function myLinkedinLogin()
 {
 	$.ajax({
-		url: "https://www.linkedin.com/oauth/v2/authorization?response_type=code&client_id=77dug7ogaz4ouh&redirect_uri=http%3A%2F%2Fsea0005.local%2Fauth%2Flinkedin%2Fcallback&state=fooobar&scope=r_liteprofile%20r_emailaddress%20w_member_social",
+		url: "https://www.linkedin.com/oauth/v2/authorization?response_type=code&client_id=77dug7ogaz4ouh&redirect_uri=https%3A%2F%2Fsea0005.sative.co.uk&state=fooobar&scope=r_liteprofile%20r_emailaddress%20w_member_social",
 		type: "GET",
 		success: function(result) {
 			console.log(result);
