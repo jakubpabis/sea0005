f1313a9134835554495cb11848600e255888c56c
Jakub Pabis <jakubpabis@Jakubs-MacBook-Pro.local>
2020-05-25 18:49:06 +0200

    test

--- build/wp-content/themes/sative/inc/fetchdata.php
+++ build/wp-content/themes/sative/inc/fetchdata.php
@@ -9,9 +9,21 @@ function userDataFetch()
 {
     $cookie = $_COOKIE['redirect_user_url'];
 
-    if( isset( $_GET['code'] ) ) {
-        $redirect = $cookie.'?code='.$_GET['code'];
+    if( isset( $_GET['code'] ) ) { ?>
+        <script>
+            $.ajax({
+                url: 'https://github.com/login/oauth/access_token?client_id=3b1b9252c021bbb321e0&client_secret=5afd24b3d4d0bf252e8034139ec6a00bc268236&code=<?= $_GET['code'] ?>',
+                type: 'POST',
+                success: function(result) {
+                    console.log(result);
+                },
+                error: function(error) {
+                    console.log(error);
+                }
+            });
+        </script>
+        <?php $redirect = $cookie.'?code='.$_GET['code'];
     }
     
-    header("Location: $redirect");
+    //header("Location: $redirect");
 }
