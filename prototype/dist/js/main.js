d4b39a95ef442ddbc2d8fe76677dce9d2dffaedf
Jakub Pabis <jakubpabis@Jakubs-MacBook-Pro.local>
2020-06-20 18:53:15 +0200

    more removes for cookies

--- prototype/dist/js/main.js
+++ prototype/dist/js/main.js
@@ -3458,15 +3458,15 @@ headTemplate:'<thead><tr><th colspan="7" class="datepicker-title"></th></tr><tr>
 })(window.Zepto || window.jQuery, window, document);
 
 // Smartsupp Live Chat script
-var _smartsupp = _smartsupp || {};
-_smartsupp.key = 'f32f591b82ffa879c325ae96ca021013ef7a7d64';
-_smartsupp.gaKey = 'UA-6319827-2';
-window.smartsupp||(function(d) {
-    var s,c,o=smartsupp=function(){ o._.push(arguments)};o._=[];
-    s=d.getElementsByTagName('script')[0];c=d.createElement('script');
-    c.type='text/javascript';c.charset='utf-8';c.async=true;
-    c.src='//www.smartsuppchat.com/loader.js?';s.parentNode.insertBefore(c,s);
-})(document);
+// var _smartsupp = _smartsupp || {};
+// _smartsupp.key = 'f32f591b82ffa879c325ae96ca021013ef7a7d64';
+// _smartsupp.gaKey = 'UA-6319827-2';
+// window.smartsupp||(function(d) {
+//     var s,c,o=smartsupp=function(){ o._.push(arguments)};o._=[];
+//     s=d.getElementsByTagName('script')[0];c=d.createElement('script');
+//     c.type='text/javascript';c.charset='utf-8';c.async=true;
+//     c.src='//www.smartsuppchat.com/loader.js?';s.parentNode.insertBefore(c,s);
+// })(document);
 function isScriptLoaded(url) {
     var scripts = document.getElementsByTagName('script');
     for (var i = scripts.length; i--;) {
