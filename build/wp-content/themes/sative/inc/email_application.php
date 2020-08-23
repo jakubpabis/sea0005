f1cea3d02694ce5eee0fbf0a6b512b3fd62e4107
Jakub Pabis <jakubpabis@Jakubs-MacBook-Pro.local>
2020-08-23 21:45:46 +0200

    add email footers

---
+++ build/wp-content/themes/sative/inc/email_application.php
@@ -0,0 +1,44 @@
+<table style="max-width: 100%; width: 100%; margin: 15px;" cellspacing="0" cellpadding="0" border="0">
+    <tbody>
+        <tr>
+            <td>
+                <h1 style="font-size: 12pt!important;">
+                    Thank you for your application for the position of <a href="{{ job_link }}" style="color: #425CBB; font-size: 12pt!important;">{{ job_title|raw }}</a>.<br/>
+                    By submitting your job application on our website, you agreed to and accepted the terms of our <a href="https://www.searchitrecruitment.com/en/privacy-policy" style="color: #425CBB; font-size: 12pt!important;">Privacy Policy</a>.
+                    <br/><br/>
+                    We will carefully consider your application and contact you if you are selected to continue in the recruitment process.
+                </h1>
+                <p style="font-size: 10pt!important;">
+                    <strong>Name:</strong> {{ name }}<br/>
+                    <strong>Email:</strong> {{ email }}<br/>
+                    <strong>Phone: </strong> {{ phone }}<br/>
+                    <strong>Address:</strong><br/>
+                    <strong>Street: </strong> {{ street }}<br/>
+                    <strong>City: </strong> {{ city }}<br/>
+                    <strong>Zip: </strong> {{zip }}<br/>
+                    <strong>Country: </strong> {{ country }}<br/>
+                    <strong>Motivation and extra details: </strong><br/> 
+                    {{ note }}
+                </p>
+                <p style="font-size: 10pt!important;">
+                    Job link: <a href="{{ job_link }}">{{ job_link }}</a>
+                </p>
+            </td>
+        </tr>
+    </tbody>
+    <tbody>
+        <tr>
+            <td style="padding-top: 40px;">
+                <p style="font-size: 10pt!important;">
+                    Keep up-to-date with our latest jobs. Follow us on social media and sign up for our newsletter/job alert!
+                </p>
+                <p style="font-size: 10pt!important;">
+                    <strong>LinkedIn: </strong><a href="https://www.linkedin.com/company/3512705">https://www.linkedin.com/company/3512705</a><br/>
+                    <strong>Facebook: </strong><a href="https://www.facebook.com/searchitrecruitment">https://www.facebook.com/searchitrecruitment</a><br/>
+                    <strong>Twitter: </strong><a href="https://twitter.com/searchitjobs">https://twitter.com/searchitjobs</a><br/>
+                    <strong>Newsletter/job alert: </strong><a href="http://jobs.searchsoftware.nl/searchit/subscribe">http://jobs.searchsoftware.nl/searchit/subscribe</a> 
+                </p>
+            </td>
+        </tr>
+    </tbody>
+</table>
---
+++ build/wp-content/themes/sative/inc/email_footer.php
@@ -0,0 +1,76 @@
+<table style="max-width: 320px; width: 100%; margin: 0 15px;" cellspacing="0" cellpadding="0" border="0">
+    <tbody>
+        <tr>
+            <td>
+                <a href="https://www.searchxrecruitment.com">
+                    <img id="TemplateLogo" data-class="external" src="../assets/img/logo-dog.png" alt="Search X Recruitment" style="display: block; margin-left: -24px; max-width: 220px;" width="220px">
+                </a>
+            </td>
+        </tr>
+        <tr>
+            <td>
+                <table cellspacing="0" cellpadding="0" border="0">
+                    <tbody>
+                        <tr>
+                            <td style="font-family: Helvetica; font-size: 9pt; padding: 2px 0;" width="70px">
+                                <font style="color: #183153; font-size: 9pt; font-family: Helvetica">
+                                    <strong>Phone:</strong>
+                                </font>
+                            </td>
+                            <td style="font-family: Helvetica; font-size: 9pt;">
+                                <font style="color: #183153; font-size: 9pt; font-family: Helvetica">
+                                    <a href="tel:+31(0)207782393" style="color: #183153; text-decoration: none;">
+                                        +31 (0) 20 - 7782393
+                                    </a>
+                                </font>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td style="font-family: Helvetica; font-size: 9pt; padding: 2px 0;" width="70px">
+                                <font style="color: #183153; font-size: 9pt; font-family: Helvetica">
+                                    <strong>Email:</strong>
+                                </font>
+                            </td>
+                            <td style="font-family: Helvetica; font-size: 9pt;">
+                                <font style="color: #183153; font-size: 9pt; font-family: Helvetica">
+                                    <a href="mailto:info@searchitrecruitment.com" style="color: #183153; text-decoration: none;">
+                                        info@searchxrecruitment.com
+                                    </a>
+                                </font>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td style="font-family: Helvetica; font-size: 9pt; padding: 2px 0;" width="70px">
+                                <font style="color: #183153; font-size: 9pt; font-family: Helvetica">
+                                    <strong>Website:</strong>
+                                </font>
+                            </td>
+                            <td style="font-family: Helvetica; font-size: 9pt;">
+                                <font style="color: #183153; font-size: 9pt; font-family: Helvetica">
+                                    <a href="https://www.searchitrecruitment.com" style="color: #183153; text-decoration: none;">
+                                        www.searchxrecruitment.com
+                                    </a>
+                                </font>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td style="font-family: Helvetica; font-size: 9pt; padding: 10px 0 1px 0;" width="70px">
+                                <font style="color: #183153; font-size: 9pt; font-family: Helvetica">
+                                    <strong>Address:<br/><br/></strong>
+                                </font>
+                            </td>
+                            <td style="font-family: Helvetica; font-size: 9pt; padding: 10px 0 1px 0;">
+                                <font style="color: #183153; font-size: 9pt; font-family: Helvetica">
+                                    <a href="https://www.google.nl/maps/place/Search+It+Recruitment/@52.3214064,4.8767044,17z/data=!3m1!4b1!4m5!3m4!1s0x47c60a6c6573e861:0x4fb3a938d9044260!8m2!3d52.3214064!4d4.8788931" style="color: #183153; text-decoration: none;">
+                                        Laan van Kronenburg 14, 1183 AS, Amstelveen, Amsterdam Area, Netherlands
+                                    </a>
+                                </font>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </td>
+        </tr>
+        
+    </tbody>
+</table>
