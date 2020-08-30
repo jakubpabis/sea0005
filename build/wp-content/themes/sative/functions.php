2339d11513d56b619310e0250284832c1e830103
Jakub Pabis <jakubpabis@Jakubs-MBP.home>
2020-08-30 16:03:13 +0200

    more

--- build/wp-content/themes/sative/functions.php
+++ build/wp-content/themes/sative/functions.php
@@ -590,6 +590,7 @@ require_once get_template_directory() . '/inc/cronjob.php';
 
 require_once get_template_directory() . '/inc/fetchdata.php';
 
+add_action( 'sative_jobs_cron_hook', 'xmlRead' );
 if ( ! wp_next_scheduled( 'sative_jobs_cron_hook' ) ) {
     wp_schedule_event( time(), 'hourly', 'sative_jobs_cron_hook' );
 }
