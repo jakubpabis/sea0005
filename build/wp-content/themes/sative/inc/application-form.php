4acb0e684a41443b2d9fc0120f9722e036022576
Jakub Pabis <jakubpabis@Jakubs-MacBook-Pro.local>
2020-09-04 17:35:07 +0200

    again

--- build/wp-content/themes/sative/inc/application-form.php
+++ build/wp-content/themes/sative/inc/application-form.php
@@ -1,8 +1,8 @@
 <?php
 
-// ini_set('display_errors', 1);
-// ini_set('display_startup_errors', 1);
-// error_reporting(E_ALL);
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 
 
 function postRequest($request, $api_key, $api_secret, $json)
