ddb73a39ec42eaaf515bc3df0c6b11967e17fe49
Jakub Pabis <jakubpabis@Jakubs-MBP.home>
2020-09-17 04:08:32 +0200

    add more recaptcha

--- build/wp-content/themes/sative/inc/application-form.php
+++ build/wp-content/themes/sative/inc/application-form.php
@@ -104,70 +104,101 @@ function add_to_queue(){
 
 function add_to_queue_cv(){
 
-    $api_key = 'XoslTEyE';
-    $api_secret = 'ZZXRgDovPQvPfLjklPLBoTAl';
+    if( isset( $_POST['g-recaptcha-response'] ) ) {
 
-    $application_data = array(
+        # Our new data
+        $data = array(
+            'secret' => '6LfL9cYZAAAAACxkyqpzP8imKOGdDQzLshPNk_vC',
+            'response' => $_POST['g-recaptcha-response']
+        );
+        # Create a connection
+        $url = 'https://www.google.com/recaptcha/api/siteverify';
+        $ch = curl_init($url);
+        # Form data string
+        $postString = http_build_query($data, '', '&');
+        # Setting our options
+        curl_setopt($ch, CURLOPT_POST, 1);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $postString);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        # Get the response
+        $responseJson = curl_exec($ch);
+        curl_close($ch);
+        $response = json_decode( $responseJson );
 
-        'name'          => isset( $_POST['cv-name'] ) ? $_POST['cv-name'] : '',
-        'email'         => isset( $_POST['cv-email'] ) ? $_POST['cv-email'] : '',
-        'date_of_birth' => isset( $_POST['cv-dob'] ) ? $_POST['cv-dob'] : '',
-        'gender'        => isset( $_POST['cv-gender'] ) ? $_POST['cv-gender'] : '',
-        'phone'         => isset( $_POST['cv-phone'] ) ? $_POST['cv-phone'] : '',
+        if( $response->success === true ) {
 
-        'location' => array(
-            'line1'   => '',
-            'line2'   => '',
-            'line3'   => '',
-            'zip'     => '',
-            'city'    => isset( $_POST['cv-city'] ) ? $_POST['cv-city'] : '',
-            'state'   => '',
-            'country' => '',
-        ),
+            $api_key = 'XoslTEyE';
+            $api_secret = 'ZZXRgDovPQvPfLjklPLBoTAl';
 
-        'sources' => array(
-            array(
-                'parent_source_id' => isset( $_POST['the_user_referrer'] ) ? $_POST['the_user_referrer'] : 'Website SIR',
-                'name' => 'Applicant' // Example: Applicant
-            ),
-        ),
+            $application_data = array(
 
-        'note' => array(
-            'text' => isset( $_POST['cv-motivation'] ) ? $_POST['cv-motivation'] : '',
-        ),
+                'name'          => isset( $_POST['cv-name'] ) ? $_POST['cv-name'] : '',
+                'email'         => isset( $_POST['cv-email'] ) ? $_POST['cv-email'] : '',
+                'date_of_birth' => isset( $_POST['cv-dob'] ) ? $_POST['cv-dob'] : '',
+                'gender'        => isset( $_POST['cv-gender'] ) ? $_POST['cv-gender'] : '',
+                'phone'         => isset( $_POST['cv-phone'] ) ? $_POST['cv-phone'] : '',
 
-        'job' => array(
-            'id' => isset( $_POST['cv-jobid'] ) ? $_POST['cv-jobid'] : 188,
-        ),
+                'location' => array(
+                    'line1'   => '',
+                    'line2'   => '',
+                    'line3'   => '',
+                    'zip'     => '',
+                    'city'    => isset( $_POST['cv-city'] ) ? $_POST['cv-city'] : '',
+                    'state'   => '',
+                    'country' => '',
+                ),
 
-        //'urls' => array('https://www.example.com/some/url/123'),
+                'sources' => array(
+                    array(
+                        'parent_source_id' => isset( $_POST['the_user_referrer'] ) ? $_POST['the_user_referrer'] : 'Website SIR',
+                        'name' => 'Applicant' // Example: Applicant
+                    ),
+                ),
 
-    );
+                'note' => array(
+                    'text' => isset( $_POST['cv-motivation'] ) ? $_POST['cv-motivation'] : '',
+                ),
 
-    $data['json'] = json_encode($application_data);
+                'job' => array(
+                    'id' => isset( $_POST['cv-jobid'] ) ? $_POST['cv-jobid'] : 188,
+                ),
 
-    if( isset( $_FILES['app-cv'] ) && $_FILES['app-cv'] ) {
+                //'urls' => array('https://www.example.com/some/url/123'),
 
-        // if( $_FILES['app-cv']['size'] <= 5248000 ) {
+            );
 
-        $uploaded_cv = realpath( $_FILES['app-cv']['tmp_name'] );
-        $cv_ext = $_FILES['app-cv']['type'];
-        $cv_name = basename( $_FILES['app-cv']['name'] );
-        $data['cv'] = curl_file_create($uploaded_cv, $cv_ext, $cv_name);
+            $data['json'] = json_encode($application_data);
 
-    } else if ( isset( $_FILES['cv-cv'] ) && $_FILES['cv-cv'] ) {
-        $uploaded_cv = realpath( $_FILES['cv-cv']['tmp_name'] );
-        $cv_ext = $_FILES['cv-cv']['type'];
-        $cv_name = basename( $_FILES['cv-cv']['name'] );
-        $data['cv'] = curl_file_create($uploaded_cv, $cv_ext, $cv_name);
-    }
+            if( isset( $_FILES['app-cv'] ) && $_FILES['app-cv'] ) {
 
-    //var_dump($application_data);
+                // if( $_FILES['app-cv']['size'] <= 5248000 ) {
 
-    $person_response = postRequest('people/add_to_queue', $api_key, $api_secret, $data);
-    //var_dump($person_response);
-    if( isset( $person_response->status ) && $person_response->status === 'ok' ) {
-        $message = sendEmailCV();
+                $uploaded_cv = realpath( $_FILES['app-cv']['tmp_name'] );
+                $cv_ext = $_FILES['app-cv']['type'];
+                $cv_name = basename( $_FILES['app-cv']['name'] );
+                $data['cv'] = curl_file_create($uploaded_cv, $cv_ext, $cv_name);
+
+            } else if ( isset( $_FILES['cv-cv'] ) && $_FILES['cv-cv'] ) {
+                $uploaded_cv = realpath( $_FILES['cv-cv']['tmp_name'] );
+                $cv_ext = $_FILES['cv-cv']['type'];
+                $cv_name = basename( $_FILES['cv-cv']['name'] );
+                $data['cv'] = curl_file_create($uploaded_cv, $cv_ext, $cv_name);
+            }
+
+            //var_dump($application_data);
+
+            $person_response = postRequest('people/add_to_queue', $api_key, $api_secret, $data);
+            //var_dump($person_response);
+            if( isset( $person_response->status ) && $person_response->status === 'ok' ) {
+                $message = sendEmailCV();
+            } else {
+                $message = 'failed';
+            }
+
+        } else {
+            $message = 'failed';
+        }
+
     } else {
         $message = 'failed';
     }
