91b6d467d75f9e8146a9d201b09f53b3d3077774
Sative <jakub@sative.co.uk>
2021-02-01 13:34:56 +0100

    again

--- build/wp-content/themes/sative/functions.php
+++ build/wp-content/themes/sative/functions.php
@@ -216,7 +216,7 @@ function sative_scripts() {
     wp_enqueue_script('sative-bootstrap', 'https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js', array(), '4.6.0', true );
     wp_enqueue_script('sative-validate', 'https://cdn.jsdelivr.net/npm/jquery-validation@1.19.3/dist/jquery.validate.min.js', array(), '1.19.3', true );
     wp_enqueue_script('sative-methods', 'https://cdn.jsdelivr.net/npm/jquery-validation@1.19.3/dist/additional-methods.min.js', array(), '1.19.3', true );
-	wp_enqueue_script('sative-app', get_template_directory_uri() . '/assets/js/main.min.js', array(), '1.1.4', true );
+	wp_enqueue_script('sative-app', get_template_directory_uri() . '/assets/js/main.min.js', array(), '1.1.5', true );
 }
 add_action( 'wp_enqueue_scripts', 'sative_scripts' );
 
