a472eff9cad8dd5384d9a044483e8ad354fab066
Sative <jakub@sative.co.uk>
2021-02-01 18:20:07 +0100

    check

--- build/wp-content/themes/sative/tpl_success.php
+++ build/wp-content/themes/sative/tpl_success.php
@@ -3,21 +3,17 @@
  * Template Name: App success
  */
 
-get_header();
-
-get_footer();
-
-if( isset( $_GET['message'] ) ) {
-    $url = preg_replace('/\?.*/', '', $_GET['ref']);
-    $redirect = $url.'?message='.$_GET['message'];
-} elseif( isset( $_GET['messagecv'] ) ) {
-    $url = preg_replace('/\?.*/', '', $_GET['ref']);
-    $redirect = $url.'?messagecv='.$_GET['messagecv'];
-} elseif( isset( $_GET['messagesb'] ) ) {
-    $url = preg_replace('/\?.*/', '', $_GET['ref']);
-    $redirect = $url.'?messagesb='.$_GET['messagesb'];
-} elseif( isset( $_GET['messagect'] ) ) {
-    $url = preg_replace('/\?.*/', '', $_GET['ref']);
-    $redirect = $url.'?messagect='.$_GET['messagect'];
-}
-header("Location: $redirect");
+ if( isset( $_GET['message'] ) ) {
+     $url = preg_replace('/\?.*/', '', $_GET['ref']);
+     $redirect = $url.'?message='.$_GET['message'];
+ } elseif( isset( $_GET['messagecv'] ) ) {
+     $url = preg_replace('/\?.*/', '', $_GET['ref']);
+     $redirect = $url.'?messagecv='.$_GET['messagecv'];
+ } elseif( isset( $_GET['messagesb'] ) ) {
+     $url = preg_replace('/\?.*/', '', $_GET['ref']);
+     $redirect = $url.'?messagesb='.$_GET['messagesb'];
+ } elseif( isset( $_GET['messagect'] ) ) {
+     $url = preg_replace('/\?.*/', '', $_GET['ref']);
+     $redirect = $url.'?messagect='.$_GET['messagect'];
+ }
+ header("Location: $redirect");
