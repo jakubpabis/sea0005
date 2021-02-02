7003a447949f0bd4364dc25da76c9017fa743007
Sative <jakub@sative.co.uk>
2021-02-02 09:00:23 +0100

    again

--- build/wp-content/themes/sative/functions.php
+++ build/wp-content/themes/sative/functions.php
@@ -178,7 +178,6 @@ add_action( 'wp_enqueue_scripts', 'smartwp_remove_wp_block_library_css' );
 
 add_action('wp_enqueue_scripts', function(){
 	if (!is_admin()) {
-		wp_deregister_script('jquery');
 		wp_deregister_script('wp-embed');
 		wp_deregister_script('wp-emoji');
 	}
