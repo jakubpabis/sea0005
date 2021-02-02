e1305a3e4bc1953a49ee7aa2ca0e96e66c61884f
Sative <jakub@sative.co.uk>
2021-02-02 11:59:44 +0100

    just in case

--- build/wp-content/themes/sative/header.php
+++ build/wp-content/themes/sative/header.php
@@ -16,6 +16,13 @@
     <meta charset="<?php bloginfo( 'charset' ); ?>">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- Google Tag Manager -->
+	<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+	new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','GTM-PMG8TTV');</script>
+	<!-- /Google Tag Manager -->
 	<!-- Bing Webmaster Tools -->
 	<meta name="msvalidate.01" content="DB26E7D9A02A63952EAC119AC5AB03FC" />
 	<!-- /Bing Webmaster Tools -->
