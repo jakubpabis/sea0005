15d0a79e459048b986a35c4da069f00629cdcf9c
Sative <jakub@sative.co.uk>
2021-05-21 10:11:11 +0100

    working

--- build/wp-content/themes/sative/template-parts/flex-content/categories-menus.php
+++ build/wp-content/themes/sative/template-parts/flex-content/categories-menus.php
@@ -24,12 +24,12 @@
 							<?php echo $column['title']; ?>
 						</h2>
 					<?php endif; ?>
-					<?php if (!empty($column['links'])) : var_dump($column['links']); ?>
+					<?php if (!empty($column['links'])) : ?>
 						<ul>
-							<?php foreach ($column['links']['link'] as $link) : ?>
+							<?php foreach ($column['links'] as $link) : ?>
 								<li>
-									<a href="<?php echo $link['url']; ?>" class="h3 font-primary text500">
-										<?php echo $link['title']; ?>
+									<a href="<?php echo $link['link']['url']; ?>" class="h3 font-primary text500">
+										<?php echo $link['link']['title']; ?>
 										<svg height="18" width="34" xmlns="http://www.w3.org/2000/svg">
 											<path d="M25.885.278l7.844 8.051a.947.947 0 01.078.091l-.078-.09a.95.95 0 01.236.412l.006.023A.895.895 0 0134 9l-.006.11-.002.011a.974.974 0 01-.02.114c-.003.007-.004.014-.006.022a.91.91 0 01-.159.323l-.01.013-.068.078-7.844 8.051a.91.91 0 01-1.307 0 .966.966 0 010-1.342l6.265-6.432H.924C.414 9.949 0 9.525 0 9s.414-.949.924-.949h29.92L24.578 1.62a.966.966 0 010-1.342.908.908 0 011.307 0z" fill="#FFFFFF" />
 										</svg>
@@ -49,10 +49,10 @@
 					<?php endif; ?>
 					<?php if (!empty($column['links'])) : ?>
 						<ul>
-							<?php foreach ($column['links']['link'] as $link) : ?>
+							<?php foreach ($column['links'] as $link) : ?>
 								<li>
-									<a href="<?php echo $link['url']; ?>" class="h3 font-primary text500">
-										<?php echo $link['title']; ?>
+									<a href="<?php echo $link['link']['url']; ?>" class="h3 font-primary text500">
+										<?php echo $link['link']['title']; ?>
 										<svg height="18" width="34" xmlns="http://www.w3.org/2000/svg">
 											<path d="M25.885.278l7.844 8.051a.947.947 0 01.078.091l-.078-.09a.95.95 0 01.236.412l.006.023A.895.895 0 0134 9l-.006.11-.002.011a.974.974 0 01-.02.114c-.003.007-.004.014-.006.022a.91.91 0 01-.159.323l-.01.013-.068.078-7.844 8.051a.91.91 0 01-1.307 0 .966.966 0 010-1.342l6.265-6.432H.924C.414 9.949 0 9.525 0 9s.414-.949.924-.949h29.92L24.578 1.62a.966.966 0 010-1.342.908.908 0 011.307 0z" fill="#FFFFFF" />
 										</svg>
