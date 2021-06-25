8389c07767e316e933b6371a5223d839da4a43b6
Sative <jakub@sative.co.uk>
2021-06-25 16:06:50 +0100

    asdasdasdasd

--- build/wp-content/themes/sative/single-jobs.php
+++ build/wp-content/themes/sative/single-jobs.php
@@ -79,7 +79,7 @@ get_header('job'); ?>
 		</div>
 	</header>
 
-	<section class="jobs__single mt-4 pt-2">
+	<section class="jobs__single mt-4 pt-2 pb-5">
 		<div class="container">
 			<?php if (isset($_GET['message'])) : ?>
 				<div class="row mb-5 mt-5">
@@ -106,78 +106,267 @@ get_header('job'); ?>
 								</div>
 							</div>
 					</div>
-				<?php endif; ?>
-				<div class="row justify-content-center">
-					<aside class="col-12">
-						<div class="row justify-content-between align-items-center">
-							<div class="col-auto">
-								<span class="mr-4 text-size-small font-secondary"><?php pll_e('Snel naar:'); ?></span>
-								<a href="" class="text-uppercase text700 mr-4 pr-3 text-size-small">Introductie</a>
-								<a href="" class="text-uppercase text700 mr-4 pr-3 text-size-small">Wat je gaat doen</a>
-								<a href="" class="text-uppercase text700 mr-4 pr-3 text-size-small">Waarom search X</a>
-								<a href="" class="text-uppercase text700 mr-4 pr-3 text-size-small">Wat je krijgt</a>
-							</div>
-							<div class="col-auto">
-								<a href="" class="btn btn__default square outline navy mr-2"><i class="fas fa-share-alt"></i></a>
-								<a href="" class="btn btn__default yellow"><?php pll_e('Solliciteren'); ?></a>
-							</div>
-						</div>
-						<hr>
-					</aside>
-					<article class="mt-5 col-lg-10 jobs__single-article">
+				</div>
+			<?php endif; ?>
+			<div class="row justify-content-center">
+				<article class="mt-5 col-lg-10 jobs__single-article">
 
-						<?php
-
-						the_content();
-
-						if (get_field('button')) :
-							echo '<a href="' . get_field('button')['url'] . '" class="btn btn__default yellow">' . get_field('button')['title'] . '</a>';
-						endif;
-
-						?>
-						<a href="" class="btn btn__default yellow mr-2"><?php pll_e('Direct solliciteren!'); ?></a>
-						<?php /* $recruiterF = get_field('recruiter_related'); ?>
-						<?php if (is_array($recruiterF) && !empty($recruiterF)) : $recruiter = $recruiterF[0]; ?>
-
-							<aside class="jobs__single-recruiter">
-								<h3><?php pll_e('Executive search consultant'); ?></h3>
-								<div class="row recruiter">
-									<div class="col-xl-3 col-lg-4 col-md-3 col-sm-4">
-										<div class="recruiter-img">
-											<img data-src="<?= get_the_post_thumbnail_url($recruiter->ID, 'medium_large'); ?>" alt="" class="lazy bg-cover">
-										</div>
-									</div>
-									<div class="col-lg-auto col-md-9 col-sm-8">
-										<div class="recruiter-text">
-											<h5>
-												<svg width="18" height="18" fill="none" xmlns="http://www.w3.org/2000/svg">
-													<path fill-rule="evenodd" clip-rule="evenodd" d="M13.365 15L9 10.64 4.635 15 3 13.367l4.365-4.36L7.36 9l.006-.006L3 4.633 4.635 3 9 7.36 13.365 3 15 4.633l-4.365 4.36.006.007-.006.006L15 13.367 13.365 15zM0 18h18V0H0v18z" fill="#FDD963" />
-												</svg>
-												<?= $recruiter->post_title; ?>
-											</h5>
-											<div class="recruiter-contact">
-												<a href="mailto:<?= get_field('email', $recruiter->ID); ?>" class="text-size-small"><?= get_field('email', $recruiter->ID); ?></a>
-												<a href="tel:<?= get_field('phone', $recruiter->ID); ?>" class="text-size-small"><?= get_field('phone', $recruiter->ID); ?></a>
-											</div>
-											<div class="recruiter-btns">
-												<a href="" class="btn btn__social notched yellow"><i class="fab fa-whatsapp"></i></a>
-												<a href="" class="btn btn__social notched yellow"><i class="fab fa-skype"></i></a>
-												<a href="<?= get_field('calendly', $recruiter->ID); ?>" class="text-size-small"><u><?php pll_e('Schedule a call or meeting'); ?></u></a>
-											</div>
+					<?php /* <aside class="dog-aside">
+							<div class="container position-absolute h-100 left-0 top-80">
+								<div class="row">
+									<div class="col-lg-11 position-absolute h-100 p-0 left-0">
+										<div class="flex_content-video_section-dog left bg-yellow w-100 h-100">
+											<svg viewBox="0 0 649.89 364.92" xmlns="http://www.w3.org/2000/svg">
+												<path d="m484.2 0-28 28.09v138.59h41.45l25.37-25.37v-62h-10.23v57.79l-19.43 19.42h-27v-124l22-22.15h151.14v42.84l-26.11 26.11h-79.19v99h-363.44l-107.65 107.52h-63.11v10.28h67.44l107.66-107.54h359.1v91.94l-37.87 46.28 26.85 27.72h-56.8v-72.26h-233.25l-46.53 44.42 26.73 27.72h-64.22v-117.4h-10.27v127.79h98.75l-36.39-37.74 36-34.4h218.9v72.27h91.33l-37.25-38.49 34.4-41.95v-194.64h73.13l32.17-32.05v-57.3h-165.68" fill="#183153" />
+												<g class="bowtie" fill="#EC6278">
+													<path d="m555.61 206.72-23.4-23.39 23.4-23.4 7.22 7.22-16.18 16.18 16.18 16.17z" />
+													<path d="m523.25 206.72-7.25-7.22 16.17-16.17-16.17-16.18 7.21-7.22 23.4 23.4-23.4 23.39" />
+												</g>
+											</svg>
 										</div>
 									</div>
 								</div>
-							</aside>
+							</div>
+							<div class="container">
+								<div class="row">
+									<div class="col-lg-9">
+										<span class="display-2 text700">
+											<?php pll_e('Waarom solliciteren via Search X Recruitment?'); ?>
+										</span>
+									</div>
+									<div class="offset-lg-3 col-lg-9 mt-5 pt-5">
+									</div>
+								</div>
+							</div>
+						</aside> */ ?>
 
-						<?php endif; */ ?>
+					<?php
 
-					</article>
-					<?php /*
+					the_content();
+
+					if (get_field('button')) :
+						echo '<a href="' . get_field('button')['url'] . '" class="btn btn__default yellow">' . get_field('button')['title'] . '</a>';
+					endif;
+
+					?>
+					<a href="" class="btn btn__default yellow mr-2"><?php pll_e('Direct solliciteren!'); ?></a>
+					<a href="" class="btn btn__default square outline navy"><i class="fas fa-share-alt"></i></a>
+
+
+				</article>
+				<?php /*
 					<aside class="col-lg-4 jobs__single-sidebar">
 						<?php get_sidebar('jobs'); ?>
 					</aside> */ ?>
+
+
+
+			</div>
+		</div>
+	</section>
+
+	<section class="jobs__single-process bg-sea py-5 my-5">
+		<div class="container py-5">
+			<div class="row">
+				<div class="col-12">
+					<h5 class="text-uppercase mb-2 text700">
+						<?php pll_e('Sollicitatie procedure'); ?>
+					</h5>
+					<h2 class="display-3 text700 mt-0 mb-5">
+						<?php pll_e('Wat staat je te wachten'); ?>
+					</h2>
 				</div>
+			</div>
+		</div>
+	</section>
+
+	<?php $recruiterF = get_field('recruiter_related'); ?>
+	<section class="jobs__single-recruiter py-5 my-5">
+
+		<div class="container py-5">
+			<div class="row">
+				<div class="col-lg-6 job-apply-form">
+					<h5 class="text-uppercase mb-2 text700">
+						<?php pll_e('Iets voor jou?'); ?>
+					</h5>
+					<h2 class="display-3 text700 mt-0 mb-5">
+						<?php pll_e('Solliciteer nu!'); ?>
+					</h2>
+					<span class="text700 text-uppercase"><?php pll_e('Solliciteer met:'); ?></span>
+					<div class="social-login">
+						<?php global $wp; ?>
+						<button type="button" class="btn btn__small pink color-white icon full text-uppercase" onclick="myLinkedinLogin( '<?= siteURL() . 'userdatafetch'; ?>', '<?= home_url($wp->request) ?>')">
+							<i class="fab fa-linkedin"></i>
+							<span>LinkedIn</span>
+						</button>
+						<button type="button" class="btn btn__small pink color-white icon full text-uppercase" onclick="myGithubLogin('<?= home_url($wp->request) ?>')">
+							<i class="fab fa-github"></i>
+							<span>Github</span>
+						</button>
+						<button type="button" class="btn btn__small pink color-white icon full text-uppercase">
+							<div class="g-signin2" data-onsuccess="onSignIn"></div>
+							<i class="fab fa-google"></i>
+							<span>Google</span>
+						</button>
+						<button type="button" class="btn btn__small pink color-white icon full text-uppercase" onclick="myFacebookLogin()">
+							<i class="fab fa-facebook-square"></i>
+							<span>Facebook</span>
+						</button>
+					</div>
+					<?php
+
+					$countries = array("Netherlands", "Netherlands Antilles", "Belgium", "Afghanistan", "Albania", "Algeria", "American Samoa", "Andorra", "Angola", "Anguilla", "Antarctica", "Antigua and Barbuda", "Argentina", "Armenia", "Aruba", "Australia", "Austria", "Azerbaijan", "Bahamas", "Bahrain", "Bangladesh", "Barbados", "Belarus", "Belize", "Benin", "Bermuda", "Bhutan", "Bolivia", "Bosnia and Herzegowina", "Botswana", "Bouvet Island", "Brazil", "British Indian Ocean Territory", "Brunei Darussalam", "Bulgaria", "Burkina Faso", "Burundi", "Cambodia", "Cameroon", "Canada", "Cape Verde", "Cayman Islands", "Central African Republic", "Chad", "Chile", "China", "Christmas Island", "Cocos (Keeling) Islands", "Colombia", "Comoros", "Congo", "Congo, the Democratic Republic of the", "Cook Islands", "Costa Rica", "Cote d'Ivoire", "Croatia (Hrvatska)", "Cuba", "Cyprus", "Czech Republic", "Denmark", "Djibouti", "Dominica", "Dominican Republic", "East Timor", "Ecuador", "Egypt", "El Salvador", "Equatorial Guinea", "Eritrea", "Estonia", "Ethiopia", "Falkland Islands (Malvinas)", "Faroe Islands", "Fiji", "Finland", "France", "France Metropolitan", "French Guiana", "French Polynesia", "French Southern Territories", "Gabon", "Gambia", "Georgia", "Germany", "Ghana", "Gibraltar", "Greece", "Greenland", "Grenada", "Guadeloupe", "Guam", "Guatemala", "Guinea", "Guinea-Bissau", "Guyana", "Haiti", "Heard and Mc Donald Islands", "Holy See (Vatican City State)", "Honduras", "Hong Kong", "Hungary", "Iceland", "India", "Indonesia", "Iran (Islamic Republic of)", "Iraq", "Ireland", "Israel", "Italy", "Jamaica", "Japan", "Jordan", "Kazakhstan", "Kenya", "Kiribati", "Korea, Democratic People's Republic of", "Korea, Republic of", "Kuwait", "Kyrgyzstan", "Lao, People's Democratic Republic", "Latvia", "Lebanon", "Lesotho", "Liberia", "Libyan Arab Jamahiriya", "Liechtenstein", "Lithuania", "Luxembourg", "Macau", "Macedonia, The Former Yugoslav Republic of", "Madagascar", "Malawi", "Malaysia", "Maldives", "Mali", "Malta", "Marshall Islands", "Martinique", "Mauritania", "Mauritius", "Mayotte", "Mexico", "Micronesia, Federated States of", "Moldova, Republic of", "Monaco", "Mongolia", "Montserrat", "Morocco", "Mozambique", "Myanmar", "Namibia", "Nauru", "Nepal", "New Caledonia", "New Zealand", "Nicaragua", "Niger", "Nigeria", "Niue", "Norfolk Island", "Northern Mariana Islands", "Norway", "Oman", "Pakistan", "Palau", "Panama", "Papua New Guinea", "Paraguay", "Peru", "Philippines", "Pitcairn", "Poland", "Portugal", "Puerto Rico", "Qatar", "Reunion", "Romania", "Russian Federation", "Rwanda", "Saint Kitts and Nevis", "Saint Lucia", "Saint Vincent and the Grenadines", "Samoa", "San Marino", "Sao Tome and Principe", "Saudi Arabia", "Senegal", "Seychelles", "Sierra Leone", "Singapore", "Slovakia (Slovak Republic)", "Slovenia", "Solomon Islands", "Somalia", "South Africa", "South Georgia and the South Sandwich Islands", "Spain", "Sri Lanka", "St. Helena", "St. Pierre and Miquelon", "Sudan", "Suriname", "Svalbard and Jan Mayen Islands", "Swaziland", "Sweden", "Switzerland", "Syrian Arab Republic", "Taiwan, Province of China", "Tajikistan", "Tanzania, United Republic of", "Thailand", "Togo", "Tokelau", "Tonga", "Trinidad and Tobago", "Tunisia", "Turkey", "Turkmenistan", "Turks and Caicos Islands", "Tuvalu", "Uganda", "Ukraine", "United Arab Emirates", "United Kingdom", "United States", "United States Minor Outlying Islands", "Uruguay", "Uzbekistan", "Vanuatu", "Venezuela", "Vietnam", "Virgin Islands (British)", "Virgin Islands (U.S.)", "Wallis and Futuna Islands", "Western Sahara", "Yemen", "Yugoslavia", "Zambia", "Zimbabwe");
+
+					?>
+					<?php
+					if (isset($_GET['app-name']) && $_GET['app-name']) {
+						$app_name = $_GET['app-name'];
+					} else {
+						$app_name = null;
+					}
+					if (isset($_GET['app-email']) && $_GET['app-email']) {
+						$app_email = $_GET['app-email'];
+					} else {
+						$app_email = null;
+					}
+					if (isset($_GET['app-location']) && $_GET['app-location']) {
+						$app_location = $_GET['app-location'];
+					} else {
+						$app_location = null;
+					}
+					if (isset($_GET['app-motivation']) && $_GET['app-motivation']) {
+						$app_motivation = $_GET['app-motivation'];
+					} else {
+						$app_motivation = null;
+					}
+					if (isset($_GET['app-url']) && $_GET['app-url']) {
+						$app_url = $_GET['app-url'];
+					} else {
+						$app_url = null;
+					}
+					?>
+					<form method="POST" action="<?php echo esc_url(admin_url('admin-post.php')); ?>" accept-charset="UTF-8" role="form" class="pt-3" id="job-application-form" enctype="multipart/form-data">
+						<div class="row align-items-center jobs__single-sidebar-inputs">
+							<div class="col-12 pb-1">
+								<div class="pretty p-icon p-round p-plain p-jelly">
+									<input type="radio" name="app-gender" value="Male">
+									<div class="state">
+										<i class="icon fas fa-mars"></i>
+										<label><?php pll_e('Male'); ?></label>
+									</div>
+								</div>
+								<div class="pretty p-icon p-round p-plain p-jelly ml-4">
+									<input type="radio" name="app-gender" value="Female">
+									<div class="state">
+										<i class="icon fas fa-venus"></i>
+										<label><?php pll_e('Female'); ?></label>
+									</div>
+								</div>
+							</div>
+							<div class="col-lg-12 col-sm-6 ugly pt-2">
+								<input class="required" type="text" name="app-name" required value="<?= $app_name ? $app_name : null; ?>">
+								<label class="ugly-label" for="app-name" <?= $app_name ? 'style="opacity: 0;"' : null; ?>><?php pll_e('Name'); ?> <span>*</span></label>
+							</div>
+							<div class="col-lg-12 col-sm-6 ugly pt-2">
+								<input class="required" type="email" name="app-email" required value="<?= $app_email ? $app_email : null; ?>">
+								<label class="ugly-label" for="app-email" <?= $app_email ? 'style="opacity: 0;"' : null; ?>><?php pll_e('Email'); ?> <span>*</span></label>
+							</div>
+							<div class="col-lg-12 col-sm-6 ugly pt-2">
+								<input type="tel" name="app-phone">
+								<label class="ugly-label" for="app-phone"><?php pll_e('Phone'); ?></label>
+							</div>
+							<div class="col-lg-12 col-sm-6 ugly pt-2">
+								<input id="app-dob-datepicker" type="text" name="app-dob" pattern="[0-9]{2}-[0-9]{2}-[0-9]{4}">
+								<label class="ugly-label" for="app-dob"><?php pll_e('Date of birth'); ?></label>
+							</div>
+							<div class="col-lg-12 col-sm-6 ugly pt-2">
+								<input type="text" name="app-city" value="<?= $app_location ? $app_location : null; ?>">
+								<label class="ugly-label" for="app-city" <?= $app_location ? 'style="opacity: 0;"' : null; ?>><?php pll_e('City'); ?></label>
+							</div>
+							<div class="col-lg-12 col-sm-6 ugly pt-2">
+								<select name="app-country">
+									<option class="default" value=""><?= pll_e('Choose your country'); ?></option>
+									<?php $i = 1;
+									foreach ($countries as $country) : ?>
+										<?php if ($i === 1 || $i === 4) : ?>
+											<optgroup>
+											<?php endif; ?>
+											<option value="<?php echo $country; ?>"><?php echo $country; ?></option>
+											<?php if ($i === 3 || $i === count($countries)) : ?>
+											</optgroup>
+										<?php endif; ?>
+									<?php $i++;
+									endforeach; ?>
+								</select>
+							</div>
+							<div class="col-12 ugly upload pt-2">
+								<label id="app-cv-label" class="full bg-white" for="app-cv"><?php pll_e('CV'); ?> <span><?= pll_e('Upload'); ?></span></label>
+								<input type="file" accept="application/pdf,application/msword,application/vnd.openxmlformats-officedocument.wordprocessingml.document" name="app-cv" onchange="getFileName(this, 'app-cv-label')">
+							</div>
+							<div class="col-12 ugly pt-2">
+								<textarea name="app-motivation"><?= $app_motivation ? $app_motivation : null; ?></textarea>
+								<label class="ugly-label" for="app-motivation" <?= $app_motivation ? 'style="opacity: 0;"' : null; ?>><?php pll_e('Motivation'); ?></label>
+							</div>
+							<div class="col-12 pt-1">
+								<div class="pretty p-icon p-plain p-jelly">
+									<input type="checkbox" name="app-pp" required class="required">
+									<div class="state">
+										<i class="icon fal fa-times"></i>
+										<label><?php pll_e('I hereby agree with the'); ?> <a href="<?php if (pll_current_language() === 'en') : ?>/privacy-policy<?php else : ?>/privacyverklaring<?php endif; ?>" style="position: relative; z-index: 9999;"><u><?php pll_e('Privacy Policy'); ?></u></a> <span class="color-pink text-size-small text600">*</span></label>
+									</div>
+								</div>
+							</div>
+							<div class="col-12 pt-4">
+								<?php get_template_part('template-parts/referrer'); ?>
+								<input type="hidden" name="postid" value="<?= get_the_ID(); ?>">
+								<input type="hidden" name="app-jobid" value="<?= get_field('job_id'); ?>">
+								<input type="hidden" name="action" value="application_form">
+								<?php wp_nonce_field('application_form', 'application_form_nonce'); ?>
+								<input type="hidden" name="jobUploadHash" id="jobUploadHash" value="<?php global $hashesForLashes;
+																																										echo $hashesForLashes['appHash']; ?>">
+								<button type="button" disabled class="fake_btn_app_loading btn btn__default pink d-none disabled"><?= pll_e('Sending, please wait...'); ?></button>
+								<button type="button" class="fake_btn_app btn btn__default yellow"><?= pll_e('Send application'); ?></button>
+								<button class="g-recaptcha app btn btn__default yellow d-none" data-sitekey="6LeA-gUaAAAAAE0620g-jcBqsq67NPiBtcj0NrCf" data-callback="onAppSubmit"><?= pll_e('Send application'); ?></button>
+							</div>
+						</div>
+					</form>
 				</div>
+				<?php if (is_array($recruiterF) && !empty($recruiterF)) : $recruiter = $recruiterF[0]; ?>
+					<div class="col-lg-5 offset-lg-1">
+						<div class="row recruiter">
+							<div class="col-xl-3 col-lg-4 col-md-3 col-sm-4">
+								<div class="recruiter-img">
+									<img data-src="<?= get_the_post_thumbnail_url($recruiter->ID, 'medium_large'); ?>" alt="" class="lazy bg-cover">
+								</div>
+							</div>
+							<div class="col-lg-auto col-md-9 col-sm-8">
+								<div class="recruiter-text">
+									<h2 class="text700">
+										<?php pll_e('Vragen?'); ?>
+									</h2>
+									<p>
+										<?php pll_e('Stel ze je persoonlijke recruiter'); ?> <u><?php echo $recruiter->post_title; ?></u>
+									</p>
+									<div class="recruiter-contact">
+										<a href="mailto:<?= get_field('email', $recruiter->ID); ?>" class="text-size-small"><?= get_field('email', $recruiter->ID); ?></a>
+										<a href="tel:<?= get_field('phone', $recruiter->ID); ?>" class="text-size-small"><?= get_field('phone', $recruiter->ID); ?></a>
+									</div>
+									<a href="" class="btn btn__default notched icon full color-white pink text-uppercase my-3">
+										<i class="fab fa-whatsapp"></i>
+										<span>Stel een vraag via Whatsapp</span>
+									</a>
+									<a href="<?= get_field('calendly', $recruiter->ID); ?>" class="btn btn__default notched icon full navy text-uppercase">
+										<i class="far fa-video"></i>
+										<?php pll_e('Plan een videocall met joost'); ?>
+									</a>
+								</div>
+							</div>
+						</div>
+					</div>
+				<?php endif; ?>
+			</div>
+		</div>
+
 	</section>
 
 	<?php
