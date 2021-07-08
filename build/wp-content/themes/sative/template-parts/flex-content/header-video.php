eb9a40548e7624eaed2d6ea9ca1ba34e046d64da
Sative <jakub@sative.co.uk>
2021-07-09 00:53:21 +0100

    sdfsdfsdfdsf

--- build/wp-content/themes/sative/template-parts/flex-content/header-video.php
+++ build/wp-content/themes/sative/template-parts/flex-content/header-video.php
@@ -1,34 +1,36 @@
-<header class="header__video bg-sea">
+<header class="header__video">
 	<div class="container-fluid px-0 justify-content-center d-flex">
 		<video muted autoplay preload="true" loop>
 			<source src="<?php echo get_sub_field('video_file'); ?>" type="video/mp4">
 			Your browser does not support the video tag.
 		</video>
 	</div>
-	<div class="container">
-		<div class="row position-absolute bottom-8vh justify-content-center w-100">
-			<div class="col-lg-7 px-0 text-center">
-				<span class="text-size-xxxxlarge d-block mb-0">
-					<?php echo get_sub_field('title'); ?>
-				</span>
-				<span class="mt-0 text-size-large d-block text400 font-secondary">
-					<?php echo get_sub_field('sub_title'); ?>
-				</span>
-				<?php if (get_sub_field('button_first') || get_sub_field('button_second')) : ?>
-					<div class="card notched bg-white d-flex align-items-center mt-4 py-4 px-3 flex-row">
-						<?php if (get_sub_field('button_first')) : ?>
-							<a href="<?php echo get_sub_field('button_first')['url']; ?>" class="btn btn__default yellow mx-2 w-50">
-								<?php echo get_sub_field('button_first')['title']; ?>
-							</a>
-						<?php endif; ?>
-						<?php if (get_sub_field('button_second')) : ?>
-							<a href="<?php echo get_sub_field('button_second')['url']; ?>" class="btn btn__default navy mx-2 w-50">
-								<?php echo get_sub_field('button_second')['title']; ?>
-							</a>
-						<?php endif; ?>
-					</div>
-				<?php endif; ?>
+	<?php if (get_sub_field('title') || get_sub_field('sub_title') || get_sub_field('button_first') || get_sub_field('button_second')) : ?>
+		<div class="container">
+			<div class="row position-absolute bottom-8vh justify-content-center w-100">
+				<div class="col-lg-7 px-0 text-center">
+					<span class="text-size-xxxxlarge d-block mb-0">
+						<?php echo get_sub_field('title'); ?>
+					</span>
+					<span class="mt-0 text-size-large d-block text400 font-secondary">
+						<?php echo get_sub_field('sub_title'); ?>
+					</span>
+					<?php if (get_sub_field('button_first') || get_sub_field('button_second')) : ?>
+						<div class="card notched bg-white d-flex align-items-center mt-4 py-4 px-3 flex-row">
+							<?php if (get_sub_field('button_first')) : ?>
+								<a href="<?php echo get_sub_field('button_first')['url']; ?>" class="btn btn__default yellow mx-2 w-50">
+									<?php echo get_sub_field('button_first')['title']; ?>
+								</a>
+							<?php endif; ?>
+							<?php if (get_sub_field('button_second')) : ?>
+								<a href="<?php echo get_sub_field('button_second')['url']; ?>" class="btn btn__default navy mx-2 w-50">
+									<?php echo get_sub_field('button_second')['title']; ?>
+								</a>
+							<?php endif; ?>
+						</div>
+					<?php endif; ?>
+				</div>
 			</div>
 		</div>
-	</div>
+	<?php endif; ?>
 </header>
