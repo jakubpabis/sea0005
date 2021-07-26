b5ee0df7a954f3caa3b26f4a1ac5841d4342ff2a
Sative <jakub@sative.co.uk>
2021-07-26 21:13:23 +0100

    pok

--- build/wp-content/themes/sative/template-parts/nav.php
+++ build/wp-content/themes/sative/template-parts/nav.php
@@ -248,7 +248,7 @@ if ($lang === 'en') {
 							<button type="button" class="navigation__lower-btn mobileMenu d-lg-none d-inline-block">
 								<i class="far fa-bars"></i>
 								<svg width="40" height="40" viewBox="0 0 40 40" fill="none" xmlns="http://www.w3.org/2000/svg">
-									<path fill-rule="evenodd" clip-rule="evenodd" d="M30.3884 34.2353L20.1176 23.9745L9.84745 34.2353L6 30.3939L16.2713 20.1321L16.2568 20.1171L16.2713 20.1027L6.00107 9.84138L9.84638 6L20.1176 16.2597L30.3889 6L34.2348 9.84138L23.964 20.1027L23.979 20.1171L23.964 20.1321L34.2353 30.3939L30.3884 34.2353ZM0 40H40V0H0V40Z" fill="#EC6278" />
+									<path fill-rule="evenodd" clip-rule="evenodd" d="M30.3884 34.2353L20.1176 23.9745L9.84745 34.2353L6 30.3939L16.2713 20.1321L16.2568 20.1171L16.2713 20.1027L6.00107 9.84138L9.84638 6L20.1176 16.2597L30.3889 6L34.2348 9.84138L23.964 20.1027L23.979 20.1171L23.964 20.1321L34.2353 30.3939L30.3884 34.2353ZM0 40H40V0H0V40Z" fill="#FDD963" />
 								</svg>
 							</button>
 						</div>
