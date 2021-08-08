1f2266eb02e3e4493930b1169121a286961c8fb6
Sative <jakub@sative.co.uk>
2021-08-08 23:59:45 +0100

    sdfgsdfg

--- build/wp-content/themes/sative/template-parts/flex-content/speech-bubbles.php
+++ build/wp-content/themes/sative/template-parts/flex-content/speech-bubbles.php
@@ -4,13 +4,13 @@
 			<?php $i = 1;
 			foreach (get_sub_field('bubbles') as $item) : ?>
 				<div class="col-md-4">
-					<div class="card bg-white d-block">
+					<div class="card p-xl-auto p-md-4 bg-white d-block">
 						<div class="row">
 							<div class="col-lg-4 col-md-5">
 								<img src="<?php echo $item['icon']['url']; ?>" alt="<?php echo $item['icon']['title']; ?>">
 							</div>
 							<div class="col-lg-8 col-md-7">
-								<span class="display-2 text700 m-0">
+								<span class="display-2 bubble text700 m-0">
 									<?php echo $item['title']; ?>
 								</span>
 								<p class="text-size-xlarge text700 font-primary mt-3 mb-0">
