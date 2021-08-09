87fdbc405518dbc94d67fd6c7d71376eb8ba38f7
Sative <jakub@sative.co.uk>
2021-08-09 09:48:09 +0100

    dfg

--- build/wp-content/themes/sative/template-parts/flex-content/speech-bubbles.php
+++ build/wp-content/themes/sative/template-parts/flex-content/speech-bubbles.php
@@ -3,22 +3,17 @@
 		<div class="row">
 			<?php $i = 1;
 			foreach (get_sub_field('bubbles') as $item) : ?>
-				<div class="col-md-4">
+				<div class="col-md-4 col-sm-8 col-10 <?php echo $i % 2 === 0 ? 'offset-md-0 offset-sm-4 offset-2' : null ?>">
 					<div class="card p-xl-auto p-md-4 bg-white d-block">
 						<div class="row">
-							<div class="col-lg-4 col-md-5">
+							<div class="col-sm-4">
 								<img src="<?php echo $item['icon']['url']; ?>" alt="<?php echo $item['icon']['title']; ?>">
 							</div>
-							<div class="col-lg-8 col-md-7">
+							<div class="col-sm-8">
 								<span class="display-2 bubble text700 m-0">
 									<?php echo $item['title']; ?>
 								</span>
 								<p class="text-size-xlarge text700 font-primary mt-3 mb-0 d-block d-md-none d-lg-block">
-									<?php echo $item['text']; ?>
-								</p>
-							</div>
-							<div class="col-12 d-none d-md-block d-lg-none">
-								<p class="text-size-xlarge text700 font-primary mt-3 mb-0">
 									<?php echo $item['text']; ?>
 								</p>
 							</div>
