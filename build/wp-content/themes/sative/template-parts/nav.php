8e59ae6e5217cc2fc0cf26d58bd8c468d24b0bd8
Sative <jakub@sative.co.uk>
2021-08-12 23:50:44 +0100

    ddfg

--- build/wp-content/themes/sative/template-parts/nav.php
+++ build/wp-content/themes/sative/template-parts/nav.php
@@ -10,7 +10,7 @@ if ($lang === 'en') {
 <nav class="navigation">
 	<div class="navigation__upper">
 		<div class="container-xl">
-			<div class="row justify-content-between align-items-center">
+			<div class="row justify-content-lg-between justify-content-center align-items-center">
 				<div class="col-auto">
 					<ul class="lang d-none d-md-flex">
 						<?php
