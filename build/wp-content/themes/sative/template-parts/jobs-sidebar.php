5df23f038ec9d9a05a3a32593fafc30dcdb74f0c
Sative <jakub@sative.co.uk>
2021-08-19 00:06:03 +0100

    sdfsdf

---
+++ build/wp-content/themes/sative/template-parts/jobs-sidebar.php
@@ -0,0 +1,51 @@
+<aside class="mt-4">
+	<div class="card bg-yellow text-navy text-left p-xl-4 p-lg-3 p-sm-4 p-3">
+		<div class="row">
+			<div class="col">
+				<h2 class="m-0">
+					<?php pll_e('Hulp nodig?'); ?>
+				</h2>
+				<p class="line-height-1 mt-2">
+					<?php pll_e('Stel je vraag aan onze specialisten'); ?>
+				</p>
+			</div>
+			<div class="col-auto px-0 mr-n4">
+				<svg class="d-block" style="width: 100px; margin-top: -100px;" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 121 266">
+					<g fill="none" fill-rule="evenodd">
+						<path fill="#183153" d="M44.16 0l7.044 7.327-20.202 18.26v86.661l-1.71-1.712h-14.59v31.73l11.069 11.107h5.448v11.08l10.394 10.383v30.172H12.681L0 218.348v22.622h51.76l-1.685-4.06H4.06v-16.94l10.364-10.903h31.25v-35.914l-2.537-2.534h21.79l11.314-13.393V55.281l23.09-23.09h21.49v-4.06H97.65L72.182 53.6v102.14l-9.14 10.819H39.206v.134l-3.929-3.924v-9.397H56.69v4.724h4.058v-8.782H27.456l-8.696-8.727v-25.992h8.85l7.657 7.656v12.071H50.96v-4.059H39.326v-9.693l-4.265-4.264V27.388l18.957-17.133 3.192 3.32-15.04 13.803 17.88 18.255 2.9-2.84-14.946-15.26 15.012-13.776-9.324-9.698h25.41v33.277h4.059V0z" />
+						<path fill="#FFF" d="M31.864 159.328l9.262 9.261-9.262 9.262-2.87-2.87 6.392-6.392-6.392-6.391z" />
+						<path fill="#FFF" d="M44.628 159.328l2.87 2.87-6.391 6.391 6.391 6.392-2.87 2.87-9.261-9.262z" />
+						<path fill="#093853" d="M72.43 174.28v86.83H61.79l-7.656-7.657v-36.632h-4.059v38.313L60.11 265.17h16.379v-90.94z" />
+					</g>
+				</svg>
+			</div>
+		</div>
+		<div class="mt-4">
+			<div class="d-flex align-items-center mb-3">
+				<a href="info@searchxrecruitment.com" class="btn btn__social notched navy smaller mr-xl-3 mr-lg-2 mr-sm-3 mr-2" target="_blank">
+					<i class="far fa-envelope"></i>
+				</a>
+				<a href="mailto:info@searchxrecruitment.com" class="text-size-small text400">
+					info@searchxrecruitment.com
+				</a>
+			</div>
+			<div class="d-flex align-items-center mb-3">
+				<a href="tel:+31207782393" class="btn btn__social notched navy smaller mr-xl-3 mr-lg-2 mr-sm-3 mr-2" target="_blank">
+					<i class="far fa-phone-alt"></i>
+				</a>
+				<a href="tel:+31207782393" class="text-size-small text400">
+					+31 (0)20 - 7782393
+				</a>
+			</div>
+			<div class="d-flex align-items-center mb-3">
+				<a href="https://wa.me/31207782393" class="btn btn__social notched navy smaller mr-xl-3 mr-lg-2 mr-sm-3 mr-2" target="_blank">
+					<i class="fab fa-whatsapp"></i>
+				</a>
+				<a href="https://wa.me/31207782393" class="text-size-small text400">
+					+31 (0)20 - 7782393
+				</a>
+			</div>
+
+		</div>
+	</div>
+</aside>
