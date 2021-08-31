5be74a204a6f21799490c0726964b116fde4305f
Sative <jakub@sative.co.uk>
2021-09-01 00:53:44 +0100

    qwe

--- build/wp-content/themes/sative/functions.php
+++ build/wp-content/themes/sative/functions.php
@@ -10,7 +10,7 @@
 
 if (!defined('_S_VERSION')) {
 	// Replace the version number of the theme on each release.
-	define('_S_VERSION', '3.9.5');
+	define('_S_VERSION', '3.9.6');
 }
 
 if (!function_exists('sative_setup')) :
