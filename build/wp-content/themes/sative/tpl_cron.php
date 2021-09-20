52b385df08bad9b3c6bafc42a7f60374dd04305b
Sative <jakub@sative.co.uk>
2021-09-20 11:49:12 +0100

    cron job hash add

--- build/wp-content/themes/sative/tpl_cron.php
+++ build/wp-content/themes/sative/tpl_cron.php
@@ -1,7 +1,13 @@
 <?php
+
 /**
  * Template Name: Cron
  */
-if( is_user_logged_in() ) {
+if (isset($GET['hash']) && $GET['hash'] === 'b31d032cfdcf47a399990a71e43c') {
+	$hash = true;
+} else {
+	$hash = false;
+}
+if (is_user_logged_in() || $hash) {
 	xmlRead();
 }
