16f43794cd87f897247e0e7d1513b948743db7c5
Sative <jakub@sative.co.uk>
2021-09-20 12:07:20 +0100

    asdf

--- build/wp-content/themes/sative/tpl_cron.php
+++ build/wp-content/themes/sative/tpl_cron.php
@@ -3,7 +3,7 @@
 /**
  * Template Name: Cron
  */
-if (isset($GET['hash']) && $GET['hash'] === 'b31d032cfdcf47a399990a71e43c') {
+if (isset($GET['hash']) && $GET['hash'] == 'b31d032cfdcf47a399990a71e43c') {
 	$hash = true;
 } else {
 	$hash = false;
