2f9e492457d3659e8f0b9c9a12154bfdfcf9833a
Sative <jakub@sative.co.uk>
2021-09-20 12:08:32 +0100

    sdfg

--- build/wp-content/themes/sative/tpl_cron.php
+++ build/wp-content/themes/sative/tpl_cron.php
@@ -8,7 +8,6 @@ if (isset($_GET['hash']) && $_GET['hash'] === 'b31d032cfdcf47a399990a71e43c') {
 } else {
 	$hash = false;
 }
-var_dump($_GET['hash']);
 if (is_user_logged_in() || $hash) {
 	xmlRead();
 }
