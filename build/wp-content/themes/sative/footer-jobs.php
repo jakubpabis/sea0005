12f557c96c3764f1e24148f92bc2a7f804301e21
Sative <jakub@sative.co.uk>
2021-10-14 21:43:46 +0100

    resolve cookies for life

--- build/wp-content/themes/sative/footer-jobs.php
+++ build/wp-content/themes/sative/footer-jobs.php
@@ -15,9 +15,9 @@ $lang = pll_current_language();
 <?php get_template_part('template-parts/search'); ?>
 </div> <!-- #wrapper -->
 <?php wp_footer(); ?>
-<?php if (isset($_COOKIE['cookies-accepted']) && $_COOKIE['cookies-accepted'] === "accepted") : ?>
-	<!-- Smartsupp Chat Tag -->
-	<script type="text/javascript">
+<!-- Smartsupp Chat Tag -->
+<script type="text/javascript">
+	if (getCookie('cookies-accepted') === 'accepted') {
 		var _smartsupp = _smartsupp || {};
 		_smartsupp.key = 'f32f591b82ffa879c325ae96ca021013ef7a7d64';
 		_smartsupp.gaKey = 'UA-6319827-2';
@@ -34,15 +34,19 @@ $lang = pll_current_language();
 			c.src = '//www.smartsuppchat.com/loader.js?';
 			s.parentNode.insertBefore(c, s);
 		})(document);
-	</script>
+	}
+</script>
 
-	<!-- LinkedIn Insight Tag -->
-	<script type="text/javascript">
+<!-- LinkedIn Insight Tag -->
+<script type="text/javascript">
+	if (getCookie('cookies-accepted') === 'accepted') {
 		_linkedin_partner_id = "14266";
 		window._linkedin_data_partner_ids = window._linkedin_data_partner_ids || [];
 		window._linkedin_data_partner_ids.push(_linkedin_partner_id);
-	</script>
-	<script type="text/javascript">
+	}
+</script>
+<script type="text/javascript">
+	if (getCookie('cookies-accepted') === 'accepted') {
 		(function() {
 			var s = document.getElementsByTagName("script")[0];
 			var b = document.createElement("script");
@@ -51,7 +55,9 @@ $lang = pll_current_language();
 			b.src = "https://snap.licdn.com/li.lms-analytics/insight.min.js";
 			s.parentNode.insertBefore(b, s);
 		})();
-	</script>
+	}
+</script>
+<?php if (isset($_COOKIE['cookies-accepted']) && $_COOKIE['cookies-accepted'] === "accepted") : ?>
 	<noscript> <img height="1" width="1" alt="" src="https://px.ads.linkedin.com/collect/?pid=14266&fmt=gif" /> </noscript>
 <?php endif; ?>
 </body>
---
+++ build/wp-content/themes/sative/template-parts/cookies-js.php
@@ -0,0 +1,63 @@
+<script type="text/javascript">
+	function setCookie(cname, cvalue, exdays) {
+		var d = new Date();
+		d.setTime(d.getTime() + exdays * 24 * 60 * 60 * 1000);
+		var expires = "expires=" + d.toUTCString();
+		document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+	}
+
+	function getCookie(cname) {
+		var name = cname + "=";
+		var decodedCookie = decodeURIComponent(document.cookie);
+		var ca = decodedCookie.split(";");
+		for (var i = 0; i < ca.length; i++) {
+			var c = ca[i];
+			while (c.charAt(0) == " ") {
+				c = c.substring(1);
+			}
+			if (c.indexOf(name) == 0) {
+				return c.substring(name.length, c.length);
+			}
+		}
+		return "";
+	}
+</script>
+<!-- Google Tag Manager -->
+<script>
+	if (getCookie('cookies-accepted') === 'accepted') {
+		(function(w, d, s, l, i) {
+			w[l] = w[l] || [];
+			w[l].push({
+				'gtm.start': new Date().getTime(),
+				event: 'gtm.js'
+			});
+			var f = d.getElementsByTagName(s)[0],
+				j = d.createElement(s),
+				dl = l != 'dataLayer' ? '&l=' + l : '';
+			j.async = true;
+			j.src =
+				'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
+			f.parentNode.insertBefore(j, f);
+		})(window, document, 'script', 'dataLayer', 'GTM-PMG8TTV');
+	}
+</script>
+<!-- /Google Tag Manager -->
+<!-- Hotjar Tracking Code for www.searchxrecruitment.com -->
+<script>
+	if (getCookie('cookies-accepted') === 'accepted') {
+		(function(h, o, t, j, a, r) {
+			h.hj = h.hj || function() {
+				(h.hj.q = h.hj.q || []).push(arguments)
+			};
+			h._hjSettings = {
+				hjid: 467663,
+				hjsv: 6
+			};
+			a = o.getElementsByTagName('head')[0];
+			r = o.createElement('script');
+			r.async = 1;
+			r.src = t + h._hjSettings.hjid + j + h._hjSettings.hjsv;
+			a.appendChild(r);
+		})(window, document, 'https://static.hotjar.com/c/hotjar-', '.js?sv=');
+	}
+</script>
