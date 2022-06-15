1eca15e318a661d7daee29cd63c78745de213ac2
Sative <jakub@sative.co.uk>
2022-06-15 19:15:59 +0200

    small

--- build/wp-content/themes/sative/template-parts/flex-content/testimonials.php
+++ build/wp-content/themes/sative/template-parts/flex-content/testimonials.php
@@ -1,25 +1,28 @@
-<section class="articles-slider flex-section">
-	<div class="container pb-5">
-		<div class="owl-carousel owl-theme articles-slider-cards testimonials">
-			<?php foreach (get_sub_field('testimonials') as $item) : ?>
-				<div class="item d-flex flex-column h-100 w-100">
-					<div class="card nothed h-100 w-100">
-						<div class="card-body pb-0">
-							<span class="d-block display-1 text700 mb-n5">
-								”
-							</span>
-							<p class="mb-4 pb-2">
-								<?php echo wp_trim_words(get_the_content(null, false, $item->ID), 40, '...'); ?>
-							</p>
-							<span class="d-block text700">
-								<?php echo get_the_title($item->ID); ?>
-							</span>
+<?php
+$testimonials = get_sub_field('testimonials');
+if (isset($testimonials) && count($testimonials) > 0) : ?>
+	<section class="articles-slider flex-section">
+		<div class="container pb-5">
+			<div class="owl-carousel owl-theme articles-slider-cards testimonials">
+				<?php foreach ($testimonials as $item) : ?>
+					<div class="item d-flex flex-column h-100 w-100">
+						<div class="card nothed h-100 w-100">
+							<div class="card-body pb-0">
+								<span class="d-block display-1 text700 mb-n5">
+									”
+								</span>
+								<p class="mb-4 pb-2">
+									<?php echo wp_trim_words(get_the_content(null, false, $item->ID), 40, '...'); ?>
+								</p>
+								<span class="d-block text700">
+									<?php echo get_the_title($item->ID); ?>
+								</span>
+							</div>
 						</div>
+						<div class="triangle"></div>
 					</div>
-					<div class="triangle"></div>
-					<?php /* <img class="w-25 testimonials-card-logo" src="<?php echo get_the_post_thumbnail_url($item->ID, 'medium'); ?>" alt="<?php echo esc_html(get_post_meta($item->ID, '_wp_attachment_image_alt', true)); ?>"> */ ?>
-				</div>
-			<?php endforeach; ?>
+				<?php endforeach; ?>
+			</div>
 		</div>
-	</div>
-</section>
+	</section>
+<?php endif; ?>
