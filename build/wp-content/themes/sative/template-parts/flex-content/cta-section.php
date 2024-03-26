f53274cc000052de3b8c650bc24f1d14cec5e13b
jakubpabis <pabis91@gmail.com>
2024-03-26 18:55:36 +0100

    more

--- build/wp-content/themes/sative/template-parts/flex-content/cta-section.php
+++ build/wp-content/themes/sative/template-parts/flex-content/cta-section.php
@@ -1,73 +1,5 @@
-<?php if (get_sub_field('contact_text')) : ?>
-	<section class="flex_content-bubbles flex-section">
-		<div class="container-lg">
-			<div class="row align-items-end">
-				<div class="col-lg-6">
-					<span class="d-block display-3 text700">
-						<?php echo get_sub_field('contact_title'); ?>
-					</span>
+<?php if (!empty(get_sub_field('section_parts')) && (get_sub_field('section_parts') === 'full' || get_sub_field('section_parts') === 'top')) : ?>
 
-
-				</div>
-				<div class="col-lg-5 col-auto offset-lg-0 offset-md-3 offset-0">
-					<h2 class="m-lg-0">
-						<?php echo get_sub_field('contact_sub_title'); ?>
-						<svg xmlns="http://www.w3.org/2000/svg" class="flex_content-bubbles-arrow position-absolute ml-4 d-sm-inline-block d-none" viewBox="0 0 118 445">
-							<g fill="none" fill-rule="evenodd" stroke="#183153" stroke-width="5">
-								<path d="M86 431.28l15 9.87 15-9.87" />
-								<path d="M101.5 441.35V26.5L77.71 3.05H0" />
-							</g>
-						</svg>
-						<span class="flex_content-bubbles-arrow">
-							<span class="arrow-right"></span>
-							<span class="arrow-line"></span>
-							<span class="arrow down"></span>
-						</span>
-					</h2>
-				</div>
-			</div>
-
-			<?php if (get_sub_field('contact_text')) : ?>
-				<div class="row d-none d-lg-block">
-					<div class="col-lg-6">
-						<p>
-							<?php echo get_sub_field('contact_text'); ?>
-						</p>
-					</div>
-				</div>
-			<?php endif; ?>
-
-			<div class="row pt-sm-5 pt-4 mt-md-4 pb-5">
-				<?php $i = 1;
-				foreach (get_sub_field('bubbles') as $item) : ?>
-					<?php
-					switch ($i) {
-						case 1:
-							$color = 'bg-sea';
-							break;
-						case 2:
-							$color = 'bg-yellow';
-							break;
-						case 3:
-							$color = 'bg-pink';
-							break;
-					}
-					?>
-					<div class="col-md-4 col-sm-8 col-10 <?php echo $i % 2 === 0 ? 'offset-md-0 offset-sm-4 offset-2' : null; ?> d-flex flex_content-bubbles-bubble-cont mb-md-0 mb-4">
-						<div class="flex_content-bubbles-bubble">
-							<h2 class="<?php echo $color; ?> px-2 d-inline-block my-0">
-								<?php echo $item['title']; ?>
-							</h2>
-							<?php echo $item['text']; ?>
-						</div>
-						<div class="triangle"></div>
-					</div>
-				<?php $i++;
-				endforeach; ?>
-			</div>
-		</div>
-	</section>
-<?php else : ?>
 	<section class="flex_content-cta flex-section">
 		<div class="container position-absolute h-100 left-0 bg-left">
 			<div class="row">
@@ -97,6 +29,10 @@
 		</div>
 	</section>
 
+<?php endif; ?>
+
+<?php if (!empty(get_sub_field('section_parts')) && (get_sub_field('section_parts') === 'full' || get_sub_field('section_parts') === 'bottom')) : ?>
+
 	<section class="flex_content-bubbles flex-section">
 		<div class="container-lg">
 			<div class="row align-items-end">
@@ -122,6 +58,15 @@
 					</h2>
 				</div>
 			</div>
+			<?php if (get_sub_field('contact_text')) : ?>
+				<div class="row d-none d-lg-block">
+					<div class="col-lg-6">
+						<p>
+							<?php echo get_sub_field('contact_text'); ?>
+						</p>
+					</div>
+				</div>
+			<?php endif; ?>
 			<div class="row pt-sm-5 pt-4 mt-md-4 pb-5">
 				<?php $i = 1;
 				foreach (get_sub_field('bubbles') as $item) : ?>
@@ -152,4 +97,5 @@
 			</div>
 		</div>
 	</section>
+
 <?php endif; ?>
