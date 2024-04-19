4acffd752a07ec5df5b8003096735f9c06782bf7
jakubpabis <pabis91@gmail.com>
2024-04-19 09:34:34 +0200

    small

--- build/wp-content/themes/sative/functions.php
+++ build/wp-content/themes/sative/functions.php
@@ -10,7 +10,7 @@
 
 if (!defined('_S_VERSION')) {
 	// Replace the version number of the theme on each release.
-	define('_S_VERSION', '4.96');
+	define('_S_VERSION', '4.97');
 }
 
 if (!function_exists('sative_setup')) :
