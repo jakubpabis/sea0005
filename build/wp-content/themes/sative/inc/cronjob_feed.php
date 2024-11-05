8060ba716af01948f2ff204719a88bf4771721ca
jakubpabis <pabis91@gmail.com>
2024-11-05 12:39:45 +0100

    new desc

--- build/wp-content/themes/sative/inc/cronjob_feed.php
+++ build/wp-content/themes/sative/inc/cronjob_feed.php
@@ -14,7 +14,6 @@ function xmlCreateFeed()
 function save_nodes_to_file()
 {
 	$days_to_remove = 10;
-	$letters = range('a', 'z');
 	$xml_file_url = 'https://jobs.searchsoftware.nl/searchit.xml';
 	$xml_hash_file_path = dirname(__DIR__, 1) . '/xml/xml_hash.txt';
 	$new_xml_file_path = dirname(__DIR__, 1) . '/xml/vacancies.xml';
@@ -67,60 +66,64 @@ function save_nodes_to_file()
 
 			if ($datediff_days <= $days_to_remove) {
 				$dom_linkedin_vacancies->appendChild($dom_linkedin_vacancy);
+				$vac_desc = $dom_linkedin_vacancy->getAttribute('description');
+				$splitted = explode('</p> ]]>', $vac_desc);
+				$new_vac_desc = $splitted[0] . '#LI-EB1' . $splitted[1];
+				$dom_linkedin_vacancy->setAttribute('description', $new_vac_desc);
 			}
 		}
 
 
 
-		// $your_date = strtotime($dom_linkedin_vacancy->getElementsByTagName('publish_date')->item(0)->nodeValue);
-		// $datediff = $now - $your_date;
-		// $datediff_days = round($datediff / (60 * 60 * 24));
+		$your_date = strtotime($dom_linkedin_vacancy->getElementsByTagName('publish_date')->item(0)->nodeValue);
+		$datediff = $now - $your_date;
+		$datediff_days = round($datediff / (60 * 60 * 24));
 
-		// if ($datediff_days >= $days_to_remove) {
-		// 	$index = floor($datediff_days / $days_to_remove) - 1;
+		if ($datediff_days >= $days_to_remove) {
+			$index = floor($datediff_days / $days_to_remove) - 1;
 
-		// 	if ($index > 25) {
-		// 		$divider = floor($index / 25);
-		// 		if ($divider > 0) {
-		// 			$index = $index - $divider * 25;
-		// 		}
-		// 	}
-		// 	if ($index >= 0) {
-		// 		$letter = $letters[$index];
-		// 	} else {
-		// 		$letter = '';
-		// 	}
+			if ($index > 25) {
+				$divider = floor($index / 25);
+				if ($divider > 0) {
+					$index = $index - $divider * 25;
+				}
+			}
+			if ($index >= 0) {
+				$letter = $letters[$index];
+			} else {
+				$letter = '';
+			}
 
-		// 	// Modify the 'id' attribute
-		// 	$vac_ID = $dom_linkedin_vacancy->getAttribute('id');
-		// 	$dom_linkedin_vacancy->setAttribute('id', $vac_ID . $letter);
-		// 	echo $vac_ID . $letter . '<br/><br/>';
-		// 	// Modify the <id> node
-		// 	$id_node = $dom_linkedin_vacancy->getElementsByTagName('id')->item(0);
-		// 	$id_node->nodeValue = $vac_ID . $letter;
+			// Modify the 'id' attribute
+			$vac_ID = $dom_linkedin_vacancy->getAttribute('id');
+			$dom_linkedin_vacancy->setAttribute('id', $vac_ID . $letter);
+			echo $vac_ID . $letter . '<br/><br/>';
+			// Modify the <id> node
+			$id_node = $dom_linkedin_vacancy->getElementsByTagName('id')->item(0);
+			$id_node->nodeValue = $vac_ID . $letter;
 
-		// 	// // Modify the <publish_date> node
-		// 	// $publish_date_node = $dom_linkedin_vacancy->getElementsByTagName('publish_date')->item(0);
-		// 	// $publish_date_node->nodeValue = date("d-m-Y H:i:s");
+			// // Modify the <publish_date> node
+			// $publish_date_node = $dom_linkedin_vacancy->getElementsByTagName('publish_date')->item(0);
+			// $publish_date_node->nodeValue = date("d-m-Y H:i:s");
 
-		// 	// Modify the <modify_date> node
-		// 	$modify_date_node = $dom_linkedin_vacancy->getElementsByTagName('modify_date')->item(0);
-		// 	$modify_date_node->nodeValue = date("d-m-Y H:i:s");
+			// Modify the <modify_date> node
+			$modify_date_node = $dom_linkedin_vacancy->getElementsByTagName('modify_date')->item(0);
+			$modify_date_node->nodeValue = date("d-m-Y H:i:s");
 
-		// 	// Modify the <url> node
-		// 	$url_node = $dom_linkedin_vacancy->getElementsByTagName('url')->item(0);
-		// 	$url_node->nodeValue = $dom_linkedin_vacancy->getElementsByTagName('url')->item(0)->nodeValue . $letter;
+			// Modify the <url> node
+			$url_node = $dom_linkedin_vacancy->getElementsByTagName('url')->item(0);
+			$url_node->nodeValue = $dom_linkedin_vacancy->getElementsByTagName('url')->item(0)->nodeValue . $letter;
 
-		// 	// Modify the <apply_url> node
-		// 	$apply_url_node = $dom_linkedin_vacancy->getElementsByTagName('apply_url')->item(0);
-		// 	$apply_url_node->nodeValue = $dom_linkedin_vacancy->getElementsByTagName('apply_url')->item(0)->nodeValue . $letter;
+			// Modify the <apply_url> node
+			$apply_url_node = $dom_linkedin_vacancy->getElementsByTagName('apply_url')->item(0);
+			$apply_url_node->nodeValue = $dom_linkedin_vacancy->getElementsByTagName('apply_url')->item(0)->nodeValue . $letter;
 
-		// 	// Modify the <job_url> node
-		// 	$job_url_node = $dom_linkedin_vacancy->getElementsByTagName('job_url')->item(0);
-		// 	$job_url_node->nodeValue = $dom_linkedin_vacancy->getElementsByTagName('job_url')->item(0)->nodeValue . $letter;
-		// }
+			// Modify the <job_url> node
+			$job_url_node = $dom_linkedin_vacancy->getElementsByTagName('job_url')->item(0);
+			$job_url_node->nodeValue = $dom_linkedin_vacancy->getElementsByTagName('job_url')->item(0)->nodeValue . $letter;
+		}
 
-		// $dom_linkedin_vacancies->appendChild($dom_linkedin_vacancy);
+		$dom_linkedin_vacancies->appendChild($dom_linkedin_vacancy);
 	}
 
 	$xml_string_linkedin = $dom_linkedin->saveXML();
