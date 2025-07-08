6ad176847df989b4f4497fbdad05641cc1f2a514
Jakub Pabis <pabis91@gmail.com>
2025-07-08 16:56:31 +0200

    againaa

--- build/wp-content/themes/sative/template-parts/footer.php
+++ build/wp-content/themes/sative/template-parts/footer.php
@@ -158,14 +158,13 @@
 				<div class="col-auto">
 					<span>© Search X Recruitment - <?php echo date("Y"); ?></span> |
 					<a href="/sitemap_index.xml"> Sitemap</a> |
-					<a href="/<?php echo $lang; ?>/<?php pll_e('fulfilled-jobs'); ?>"> <?php pll_e('Fulfilled jobs'); ?></a>
+					<a href="/<?php echo $lang; ?>/<?php pll_e('fulfilled-jobs'); ?>"> <?php pll_e('Fulfilled jobs'); ?></a> |
 					<?php
 					if ($lang === 'en') {
 						$link = 'terms-conditions/';
 					} else {
 						$link = 'algemene-voorwaarden/';
 					}
-
 					?>
 					<a href="/<?php echo $lang; ?>/<?php echo $link; ?>"> <?php pll_e('Terms & Conditions'); ?></a>
 				</div>
